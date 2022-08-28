62cdeb70bd4279ae2cecf58071e37665262b1449
Pourya Mansouri <pourya.mansouri@hotmail.com>
2022-08-28 09:40:44 +0430

    print ticket

--- events/serializers.py
+++ events/serializers.py
@@ -4,25 +4,37 @@ from rest_framework import serializers
 from events.models import Ticket
 from events.tasks import celery_successful_ticket_sms, ticket_sms
 
+
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = '__all__'
 
-    def create (self, validated_data):
-        try : 
-            #check if user buy ticket before with same national code
+    def create(self, validated_data):
+        try:
+            # check if user buy ticket before with same national code
             national_code = validated_data['national_code']
             events = validated_data['event']
             ticket = Ticket.objects.get(national_code=national_code, event=events)
             raise serializers.ValidationError({'detail': 'برای این کدملی بلیط در این رویداد ثبت شده است'})
         except Ticket.DoesNotExist:
             ticket = Ticket(**validated_data)
-            #add who buy ticket
+            # add who buy ticket
             ticket.user = self.context['request'].user
             ticket.status = Ticket.STATUS.SUCCESSFUL
             ticket.save()
             ticket.event_capacity_reducer()
             ticket_sms(ticket.phone, ticket.national_code)
-            
+
         return ticket
+
+
+class SendTicketInfoSerializer(serializers.ModelSerializer):
+    code = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Ticket
+        fields = ('full_name', 'phone', 'national_code', 'code')
+
+    def get_code(self, obj):
+        return obj.unique_code
